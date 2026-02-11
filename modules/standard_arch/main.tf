6a864ebd333e19739ceebd3c4dfb7d07f3f32feb
Ignacio Morales C. <ignacio.morales@live.com>
2026-02-11 17:07:36 -0500

    Fix syntax error: unclosed app_insights block

--- modules/standard_arch/main.tf
+++ modules/standard_arch/main.tf
@@ -125,6 +125,8 @@ resource "azurerm_application_insights" "appinsights" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   workspace_id        = azurerm_log_analytics_workspace.law.id
+  application_type    = "web"
+}
 
 # Data Engineering: Storage Containers
 resource "azurerm_storage_container" "input" {
