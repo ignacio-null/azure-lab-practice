699d17ed81b1e29b66b3f63a8c51c11004dcbe79
Ignacio Morales C. <ignacio.morales@live.com>
2026-02-11 13:09:01 -0500

    Agrega CI/CD workflow y backend remoto

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "practice-lab-mgmt-rg"
+    storage_account_name = "practicelabtfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
